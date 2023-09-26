2a9216b4b9986b93fb20dd021afecc4cf74d3be4
Daniele Susino <susino.daniele@outlook.com>
2023-09-26 15:53:20 +0200

    Try DB Connection

--- main.py
+++ main.py
@@ -20,24 +20,33 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-# Configurazione DB
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="yourusername",
-  password="yourpassword"
-)
+mydb = None
 
 # Creiamo la funzione start che se richiamata stampa a video "BUONGIORNISSIMO, KAFFÈ!?"
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="BUONGIORNISSIMO, KAFFÈ!?")
 
 if __name__ == "__main__":
+
     load_dotenv(find_dotenv()) # Carichiamo il file di ambiente dove è salvato il token
     application = ApplicationBuilder().token(os.environ.get("BOT_TOKEN")).build() # Ci impossessiamo del bot con il nostro TOKEN
     
+    # Prova a connetterti al DB, altrimenti dai errore
+    try:
+        mydb = mysql.connector.connect(
+            host=os.environ.get("DB_HOST"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD")
+        )
+    except:
+        print("Non è stato possibile connettersi al DB.")
+        exit(-1)
+
     # Creiamo il comando start e lo aggiungiamo ai comandi runnabili
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
     
     application.run_polling() # Inizializza l'app
+
+    print(mydb)
     
