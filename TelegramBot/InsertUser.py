ea34256d11873b407c1724279b07d6947b73ae24
Daniele Susino <susino.daniele@outlook.com>
2023-09-26 21:33:57 +0200

    BugFix (vedi desc.)
    Fixxato un bug dove gli inserimenti di un utente leakkavano in quelli di un altro.
    Colpa delle variabili globali, sistemato con una struct ed un dizionario.

--- TelegramBot/InsertUser.py
+++ TelegramBot/InsertUser.py
@@ -1,56 +1,58 @@
 from telegram import Update
 from telegram.ext import ContextTypes, ConversationHandler, CommandHandler, MessageHandler, filters
+from dataclasses import dataclass
 
 # Gli stati della conversazione
-ID_TELEGRAM, NOME_COMPLETO, SALDO = range(3)
+NOME_COMPLETO, SALDO = range(2)
 
-# I valori che vogliamo memorizzare
-idTelegram = ""
-nomeCompleto = ""
-saldo = 0
+# Creiamo una struct in modo tale da potere memorizzare tutte le informazioni 
+# in base ad una chiave (id Telegram dello scrittore)
+@dataclass
+class User:
+    Nome: str
+    Saldo: float
+
+# Inizializziamo il dizionario
+usersAndValues = {}
 
 # Chiediamo all'utente di Inserire il tag del nuovo utente
 async def AddUser(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Inserisci il tag dell'utente: ")
-    return ID_TELEGRAM # Ritorniamo lo stato ID_TELEGRAM per andare in quella funzione
-
-# Memorizziamo il messaggio mandato dall'utente come username e chiediamo il nome completo
-async def InsertIDTelegram(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global idTelegram
-    idTelegram = update.message.text
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Inserisci il nome completo dell'utente: ")
     
-    await update.message.reply_text(f"Ora inserisci il nome completo dell'utente:")
-    return NOME_COMPLETO
+    usersAndValues[update.message.chat_id] = User("", 0)
 
-# Memorizziamo il messaggio mandato dall'utente come nomecompleto e chiediamo il saldo
+    return NOME_COMPLETO # Ritorniamo lo stato ID_TELEGRAM per andare in quella funzione
+
+# Memorizziamo il messaggio mandato dall'utente come nome completo e chiediamo il saldo
 async def InsertNomeCompleto(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global nomeCompleto
-    nomeCompleto = update.message.text
+    
+    usersAndValues[update.message.chat_id].Nome = update.message.text
 
-    await update.message.reply_text(f"Ora inserisci il saldo iniziale di {nomeCompleto}:")
+    await update.message.reply_text(f"Ora inserisci il saldo inziale dell'utente:")
     return SALDO
 
 # Memorizziamo il messaggio mandato dall'utente come saldo
 async def InsertSaldo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global saldo
     saldo = update.message.text
 
     # Ma solo se è un numero, altrimenti ripeti
-    if(saldo.isdigit()):
+    if(not saldo.isdigit()):
         await update.message.reply_text(f"{update.message.text} non è un numero, riprova!")
         return SALDO
 
     saldo = float(saldo)
+    usersAndValues[update.message.chat_id].Saldo = saldo
 
-    await update.message.reply_text(f"Ottimo, l'utente {nomeCompleto} con ID: {idTelegram}, verrà memorizzato con saldo pari a: {saldo}€")
+    await update.message.reply_text(f"Ottimo, l'utente {usersAndValues[update.message.chat_id].Nome}, verrà memorizzato con saldo pari a: {usersAndValues[update.message.chat_id].Saldo}€")
     return ConversationHandler.END
+
+# TODO: MANDARE RICHIESTA AL DB ED UNA VOLTA ENTRATO RIMUOVERE L'UTENTE DAL DIZIONARIO
 
 def CreateAddUserHandler(Cancel):
     return ConversationHandler(
         entry_points=[CommandHandler('add', AddUser)],
         states={
             # Dipende dallo stato nella quale ci troviamo, richiama una funzione specifica
-            ID_TELEGRAM: [MessageHandler(filters.TEXT & ~filters.COMMAND, InsertIDTelegram)],
             NOME_COMPLETO: [MessageHandler(filters.TEXT & ~filters.COMMAND, InsertNomeCompleto)],
             SALDO: [MessageHandler(filters.TEXT & ~filters.COMMAND, InsertSaldo)]
         },
