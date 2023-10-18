245134d835841d33c019e8e48703c6fb302f3586
DeCode <susino.daniele@outlook.com>
2023-10-18 16:30:37 +0200

    Update main.py

--- Api/main.py
+++ Api/main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from Modules.Configs import LoadConfigs
 from Modules.Database import GetAulette
 
-app = FastAPI()
 
+app = FastAPI()
 ######## EVENTI ########
 
 @app.on_event("startup")
@@ -23,3 +23,11 @@ async def suca():
 @app.post("/pay/{idAuletta}")
 async def pay(idAuletta : int):
     return {"message": [["Auletta: ", idAuletta]]}
+
+@app.post("/porcodue")
+async def porcodue(request: Request):
+    a = request.query_params['a']
+    b = request.query_params['b']
+    return { {"message_a" : a }, {"message_b" : b } }
+
+
