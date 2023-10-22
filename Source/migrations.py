cf88c27fb9e85690ee883a6cf795098b3c2d3a63
Daniele Susino <susino.daniele@outlook.com>
2023-10-22 20:35:59 +0200

    Try except

--- Source/migrations.py
+++ Source/migrations.py
@@ -1,9 +1,12 @@
 import vlc
 
-# Specifica il percorso del tuo file MP3
-file_path = '../Resources/Audio/BGMusic.mp3'
+try:
+    # Specifica il percorso del tuo file MP3
+    file_path = '../Resources/Audio/BGMusic.mp3'
 
-player = vlc.MediaPlayer(file_path)
-player.play()
+    player = vlc.MediaPlayer(file_path)
+    player.play()
+except:
+    print("Il tuo sistema non può mandare in output la musica. Sei in connessione SSH?")
 
-input()
+input("Inserisci la tua scelta: ")
