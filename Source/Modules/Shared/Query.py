efc332c1e55c835dd90b1aaaf4e1512a337c2a8b
Daniele Susino <susino.daniele@outlook.com>
2023-11-15 22:43:53 +0100

    Update Query.py

--- Source/Modules/Shared/Query.py
+++ Source/Modules/Shared/Query.py
@@ -140,7 +140,7 @@ def GetProdotti(idAuletta : int) -> str:
 
     arr = []
 
-    res2 = session.query(Magazzino, Prodotto).join(Prodotto, Prodotto.ID_Prodotto == Magazzino.ID_Prodotto, isouter=True).filter(Magazzino.ID_Auletta == idAuletta).all()
+    res2 = session.query(Magazzino, Prodotto).join(Prodotto, Prodotto.ID_Prodotto == Magazzino.ID_Prodotto).filter(Magazzino.ID_Auletta == idAuletta).all()
 
     print(res2)
     
