f9de610f68d841caefd71184a36103446ec027f5
Daniele Susino <susino.daniele@outlook.com>
2023-11-15 20:09:35 +0100

    Update Query.py

--- Source/Modules/Shared/Query.py
+++ Source/Modules/Shared/Query.py
@@ -137,7 +137,7 @@ def DecurtaMagazzino(idProdotto : int, idAuletta : int, quantita : int):
 def GetProdotti(idAuletta : int) -> str:
     """WEB_API: Dato l'ID di un'auletta, restituisce i suoi prodotti"""
 
-    return session.query(Magazzino).with_entities(Magazzino.ID_Prodotto, Magazzino.costo).filter(Magazzino.ID_Auletta == f"{idAuletta}").all()
+    return session.query(Magazzino.ID_Prodotto, Magazzino.costo).filter(Magazzino.ID_Auletta == f"{idAuletta}").all()
 
 #endregion
 
