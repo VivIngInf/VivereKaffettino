98d1d6a403423ff220d23f998c49a59f3ed1239b
Daniele Susino <susino.daniele@outlook.com>
2024-03-12 20:06:09 +0100

    Provo a fixxare cache

--- Source/Modules/Database/connect.py
+++ Source/Modules/Database/connect.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 
-engine = create_engine("sqlite:///database.sqlite", echo=True)
+engine = create_engine("sqlite:///database.sqlite", isolation_level="READ UNCOMMITTED", echo=True)
 
 """with engine.connect() as connection:
     result = connection.execute()"""
