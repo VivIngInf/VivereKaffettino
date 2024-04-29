1c805ac1cbff25f21d0b7a90162658783ec782a9
Daniele Susino <susino.daniele@outlook.com>
2024-04-29 16:08:32 +0200

    Finalmente fatto

--- Source/telegramBot.py
+++ Source/telegramBot.py
@@ -67,7 +67,7 @@ def main() -> None:
 
     job_queue = application.job_queue
     job_queue.run_daily(SendDailyResoconto,
-                        time=datetime.time(hour=16, minute=3, second=0, tzinfo=pytz.timezone('Europe/Rome')))
+                        time=datetime.time(hour=23, minute=55, second=0, tzinfo=pytz.timezone('Europe/Rome')))
     job_queue.run_daily(FlushBirthdayList, time=datetime.time(hour=23, minute=59, second=0, tzinfo=pytz.timezone('Europe/Rome')))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
