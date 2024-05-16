54f4cfdb739f69b6d3c7a12996b1b46c5e014b4a
Daniele Susino <susino.daniele@outlook.com>
2024-05-16 22:08:37 +0200

    Update persistent.py

--- Source/Modules/Interface/persistent.py
+++ Source/Modules/Interface/persistent.py
@@ -13,14 +13,14 @@ def CreatePersistent():
 
     ingegneria = Auletta(
         ID_Auletta=1,
-        Nome="Ingegneria - ED. 8",
+        Nome="Ingegneria-ED.8",
         DebitoMax=3,
         ID_GruppoTelegram="-1002059887014"
     )
 
     deim = Auletta(
         ID_Auletta=2,
-        Nome="Ingegneria - DEIM",
+        Nome="Ingegneria-DEIM",
         DebitoMax=3,
         ID_GruppoTelegram="-1002059887014"
     )
