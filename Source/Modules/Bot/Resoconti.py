489bd3d48febf5c15b2c41b9d0eac456cff80bee
Daniele Susino <susino.daniele@outlook.com>
2024-05-23 15:36:34 +0200

    Resoconti colonne larghezza
    Automaticamente imposta la larghezza delle colonne.

--- Source/Modules/Bot/Resoconti.py
+++ Source/Modules/Bot/Resoconti.py
@@ -17,28 +17,13 @@ async def SendDailyResoconto(context: CallbackContext):
     rowsRicaricheDataframe = []
 
     for id_o, id_t, u, na, pr, c, pa, do in rowsOperazioni:
-        tempArr = []
-        tempArr.append(id_o)
-        tempArr.append(id_t)
-        tempArr.append(u)
-        tempArr.append(na)
-        tempArr.append(pr)
-        tempArr.append(c)
-        tempArr.append(pa)
-        tempArr.append(datetime(do.year, do.month, do.day, do.hour, do.minute, do.second, do.microsecond))
+        date = datetime(do.year, do.month, do.day, do.hour, do.minute, do.second, do.microsecond)
+        tempArr = [id_o, id_t, u, na, pr, c, pa]        
         rowsOperazioniDataframe.append(tempArr)
 
     for id_r, id_b, u_b, id_a, u_a, i, sp, sd, dt_r in rowsRicariche:
-        tempArr = []
-        tempArr.append(id_r)
-        tempArr.append(id_b)
-        tempArr.append(u_b)
-        tempArr.append(id_a)
-        tempArr.append(u_a)
-        tempArr.append(i)
-        tempArr.append(sp)
-        tempArr.append(sd)
-        tempArr.append(datetime(dt_r.year, dt_r.month, dt_r.day, dt_r.hour, dt_r.minute, dt_r.second, dt_r.microsecond))
+        date = datetime(dt_r.year, dt_r.month, dt_r.day, dt_r.hour, dt_r.minute, dt_r.second, dt_r.microsecond)
+        tempArr = [id_r, id_b, u_b, id_a, u_a, i, sp, sd, date]
         rowsRicaricheDataframe.append(tempArr)
 
     # Creazione del DataFrame
@@ -50,6 +35,25 @@ async def SendDailyResoconto(context: CallbackContext):
     with pandas.ExcelWriter(excel_buffer, engine='xlsxwriter') as writer:
         dfOperazioni.to_excel(writer, sheet_name='Operazioni', index=False)
         dfRicariche.to_excel(writer, sheet_name='Ricariche', index=False)
+
+        worksheet_operazioni = writer.sheets['Operazioni']
+        worksheet_ricariche = writer.sheets['Ricariche']
+
+        # Imposta la larghezza delle colonne per 'Operazioni'
+        for i, col in enumerate(dfOperazioni.columns):
+            max_len = max(
+                dfOperazioni[col].astype(str).map(len).max(),  # lunghezza massima dei dati
+                len(col)  # lunghezza del nome della colonna
+            ) + 2  # margine extra
+            worksheet_operazioni.set_column(i, i, max_len)
+        
+        # Imposta la larghezza delle colonne per 'Ricariche'
+        for i, col in enumerate(dfRicariche.columns):
+            max_len = max(
+                dfRicariche[col].astype(str).map(len).max(),  # lunghezza massima dei dati
+                len(col)  # lunghezza del nome della colonna
+            ) + 2  # margine extra
+            worksheet_ricariche.set_column(i, i, max_len)
 
     excel_buffer.seek(0)  # Riposiziona il cursore all'inizio del buffer
 
@@ -65,20 +69,28 @@ async def SendUsersResoconto(context: CallbackContext):
     rowsDataframe = []
 
     for id_t, id_c, u, s, n, v, a in rows:
-        tempArr = []
-        tempArr.append(id_t)
-        tempArr.append(id_c)
-        tempArr.append(u)
-        tempArr.append(s)
-        tempArr.append(n)
-        tempArr.append(v)
-        tempArr.append(a)
+        tempArr = [id_t, id_c, u, s, n, v, a]
         rowsDataframe.append(tempArr)
 
     df = pandas.DataFrame(rowsDataframe, columns=columns)
 
     excel_buffer = BytesIO()
-    df.to_excel(excel_buffer, index=False)
+    
+    with pandas.ExcelWriter(excel_buffer, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Utenti')
+        
+        worksheet = writer.sheets['Utenti']
+        
+        # Imposta la larghezza delle colonne
+        for i, col in enumerate(df.columns):
+            max_len = max(
+                df[col].astype(str).map(len).max(),  # lunghezza massima dei dati
+                len(col)  # lunghezza del nome della colonna
+            ) + 2  # margine extra
+            worksheet.set_column(i, i, max_len)
+    
+    
     excel_buffer.seek(0)  # Riposiziona il cursore all'inizio del buffer
 
     await context.bot.send_document(chat_id=f"{GetChannelID()}", document=excel_buffer, filename=f'ResocontoUtenti-{datetime.date(datetime.now())}.xlsx', caption=f"Resoconto utenti del {datetime.date(datetime.now())} inviato!")
+    excel_buffer.close()
